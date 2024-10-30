4766fd9027051ec2163da3530e3d71f8bd46936e
Apiker <63703342+Apiker@users.noreply.github.com>
2024-10-30 12:34:36 +0100

    Update isplaindromestring.py

--- isplaindromestring.py
+++ isplaindromestring.py
@@ -18,9 +18,20 @@ def isPalindromeInterative(s):
         result = result and s[i] == s[L-i]
     return result
 
+def isPalindromeWhile(s):
+    L = len(s)
+    result = True
+    left = 0
+    right = L-1
+    while left < right:
+        result = result and s[right] == s[left]
+        left = left + 1
+        right = right - 1
+    return result
+
+
 
 s = input("Write something: ")
 print("Recursion: ", s, isPalindromeRecursion(s))
 print("interactive: ", s, isPalindromeRecursion(s))
-
-    
+print("While: ", s, isPalindromeWhile(s))
